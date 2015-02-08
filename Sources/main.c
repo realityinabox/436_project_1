904e4306cf9e71c3546c39735eeb805f7d550071
Steve Bedford <Steve@DFTBA.local>
2015-02-08 08:36:19 -0500

    separated main.c out into seperate functions

--- Sources/main.c
+++ Sources/main.c
@@ -73,32 +73,38 @@ extern volatile int counter, pb, enc_write_flag, adc1_flag, timer_1s;
 #define RES 9890 // ohms
 #define LIGHT 0
 #define TEMP 1
+#define ADC_DIVS 19.8591
+#define ADC_FULL_VOL 3300
+#define CELCIUS 0
+#define FAREN 1
+
+void update_temp_display(void);
+void update_lux_display(void);
+void read_light(void);
+void read_temp(void);
+void check_enc_pb(void);
 
 /* User includes (#include below this line is not maintained by Processor Expert) */
-
-/*lint -save  -e970 Disable MISRA rule (6.3) checking. */
-int main(void)
-/*lint -restore Enable MISRA rule (6.3) checking. */
-{
-	/* Write your local variable definition here */
-
-	char *str[10], *int_str[4], *dec_str[4], *lux_str[10], *res_str[10];
+char *str[10], *int_str[4], *dec_str[4], *lux_str[10], *res_str[10];
 	int count = 0, conv_type = 1;
 	int char_col;
 	int button_pressed, enc_pb_flag = 0, temp_flag = 0, lux_flag = 1, mode = 1, hold = 0;
 	unsigned short Val[2], full = 0xFFFF, temp_adc, light;
 	float temp_C, temp_F, temp_mV, light_mV, resistance, light_ratio;
 	int temp_int, temp_dec, x_pos;
-
-
+/*lint -save  -e970 Disable MISRA rule (6.3) checking. */
+int main(void)
+/*lint -restore Enable MISRA rule (6.3) checking. */
+{
+	/* Write your local variable definition here
 	/*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
 	PE_low_level_init();
 	/*** End of Processor Expert internal initialization.                    ***/
 
-	enc_write_flag = 0;
+	temp_flag = 1;
+	lux_flag = 1;
 	/* Write your code here */
 	// Create infinite loop
-
 	while(1){
 
 		// ADC READS
@@ -112,188 +118,41 @@ int main(void)
 			if (conv_type == TEMP){
 				(void)AD1_MeasureChan(FALSE, 1);
 			}
-
-			//(void)AD1_Measure(FALSE);
-			// clear flag
+			// reset timer flag
 			timer_1s = 0;
-			// toggle LEDAD2
-			//rLED_NegVal();
+
 		}
 		// if LIGHT conversion is completed
 		if(adc1_flag == 1 && conv_type == LIGHT){
-			// reset conversion done flag
+			read_light();
+			// reset conversion flags
 			adc1_flag = 0;
-			// read values
-			(void)AD1_GetChanValue16(LIGHT, &Val[LIGHT]);
-			// assign values to local variables
-			light = Val[LIGHT] ;
-
-			// convert light reading from adc to mV
-			light_mV = light / 19.8591;
-			light_ratio = light_mV / 3300;
-
-			// calculate the resistance value
-			resistance = (RES * light_ratio) / ( 1 - light_ratio);
-
-			// convert light reading from adc to mV
-			light_mV = light / 19.8591;
-			light_ratio = light_mV / 3300;
-
-			// set lux_flag
-			lux_flag = 1;
-			// set next conversion to be for temperature
 			conv_type = TEMP;
 
 		}
 		// if TEMPERATURE conversion is done
 		if (adc1_flag == 1 && conv_type == TEMP){
-			// reset conversion done flag
+			read_temp();
+			// reset conversion flags
 			adc1_flag = 0;
-			// read values
-			(void)AD1_GetChanValue16(TEMP, &Val[TEMP]);
-			// assign values to local variables
-
-			temp_adc = Val[TEMP] ;
-
-			// convert temp_adc to mV by scaling 2^16 over 3.3 volts
-			temp_mV = temp_adc / 19.8591;
-			temp_C = (temp_mV - 500) / 10;
-			temp_F = ((9 * temp_C) / 5) + 32;
-
-			// set the temp_flag to signal display to update
-			temp_flag = 1;
-			// set next conversion to light
 			conv_type = LIGHT;
 		}
 		// END ADC READS
 
-		// ROTARY ENCODER PUSH BUTTON DEBOUNCE
-		// get the push button state
-		button_pressed = ENC_PB_GetVal();
-		// if button is pressed
-		if (button_pressed == 0){
-			// increment counter
-			count++;
-			// might need to guard against over-flow here, incase the button is held down and we keep incrementing
-		}
-		// wait for counter to count up to 30,000 (ie, debounced)
-		if (count >= 15000){
-			// update button status
-			button_pressed = ENC_PB_GetVal();
-			// if button is pressed, set the flag
-			if (button_pressed == 0)
-				enc_pb_flag = 1;
-			// if button is released
-			if (button_pressed == 1)
-			{
-				// clear the counter, the push button flag, and the hold flag
-				count = 0;
-				enc_pb_flag = 0;
-				hold = 0;
-			}
-		}
-		// if the push button flag is set and the hold flag is clear
-		if (enc_pb_flag == 1 && hold == 0){
-			// clear the pushbutton flag, toggle the mode, set the hold flag
-			enc_pb_flag = 0;
-			mode ^= 1;
-			hold = 1;
-			// flag the display to update temperature
-			temp_flag = 1;
-			lux_flag = 1;
-			// invert the display
-			//PDC1_SetMode(mode);
-		}
-		// END DEBOUNCE
+		check_enc_pb();
 
 		// UPDATE THE DISPLAY
-
-		// WRITE NEW ENCODER COUNTERS DATA
-		if (enc_write_flag == 1){
-			// Convert the int counter value to a string for writing to display
-			sprintf(str, "%0.3d", counter);
-			// write the counter value to the display
-			PDC1_WriteStringBigger("Count:", 0, 0);
-			// clear the old value
-			PDC1_ClearLine(2);
-			PDC1_ClearLine(3);
-			// write the count value
-			PDC1_WriteStringBigger(str, 10, 2);
-			// reset the write flag
-			enc_write_flag = 0;
-		}
 		// WRITE NEW TEMPERATURE DATA and LUX DATA
 		if (temp_flag == 1 && lux_flag == 1){
-			// Convert the int counter value to a string for writing to display
-			if (mode == 1){
-				// get the whole number portion of temperature float
-				temp_int = floorf(temp_F);
-				// get the first two decimal places
-				temp_dec = (temp_F - temp_int) * 100;
-				// write the temperature type to display
-				PDC1_SetPos(0,0);
-				PDC1_WriteString("Temp(F):");
-			}
-			if (mode == 0){
-				// get the whole number portion of temperature
-				temp_int = floorf(temp_C);
-				// get the first two decimal places of temperature
-				temp_dec = (temp_C - temp_int) * 100;
-				// write temperature type to display
-				PDC1_SetPos(0,0);
-				PDC1_WriteString("Temp(C):");
-			}
-			// convert the temperatures to strings
-			sprintf(int_str, "% d", (int)temp_int);
-			sprintf(dec_str, "%.2d", (int)temp_dec);
-			// try to lop off the leading zero
-			if (int_str[0] == "0"){
-				int_str[0] = " ";
-			}
-			// clear the old value
-			PDC1_ClearLine(1);
-			PDC1_ClearLine(2);
-			// set initial x position
-			x_pos = 0;
-			// write integer value
-			x_pos = PDC1_WriteStringBigger(int_str, x_pos, 1);
-			// write the decimal place
-			x_pos = PDC1_WriteStringBigger(".", x_pos, 1);
-			// write the decimals
-			x_pos = PDC1_WriteStringBigger(dec_str, x_pos, 1);
-
+			// update display
+			update_temp_display();
 			// reset the write flag
 			temp_flag = 0;
-		//}
-		// WRITE LUMINANCE DATA
-		//if (lux_flag == 1){
-			PDC1_ClearLine(3);
-			PDC1_ClearLine(4);
-			PDC1_ClearLine(5);
-
-			if (light > 40000){
-				PDC1_weather_image(NIGHT, 60, 3);
-			}
-			else if (light <= 40000 && light > 8000){
-				PDC1_weather_image(CLOUDY, 60, 3);
-			}
-			else if (light <= 8000){
-				PDC1_weather_image(SUNNY, 60, 3);
-			}
-			sprintf(lux_str, "%d", (int)light);
-			sprintf(res_str, "%d", (int)resistance);
-			PDC1_SetPos(0,3);
-			PDC1_WriteString("ADC:");
-			PDC1_WriteString(lux_str);
-			PDC1_SetPos(0,4);
-			PDC1_WriteString("Res:");
-			PDC1_WriteString(res_str);
+			// update display
+			update_lux_display();
 			lux_flag = 0;
-
 		}
 		// END DISPLAY UPDATE
-
-
 	}
 
 	/*** Don't write any code pass this line, or it will be deleted during code generation. ***/
@@ -306,7 +165,186 @@ int main(void)
   for(;;){}
   /*** Processor Expert end of main routine. DON'T WRITE CODE BELOW!!! ***/
 } /*** End of main routine. DO NOT MODIFY THIS TEXT!!! ***/
+/*
+ *  void read_light(void)
+ *  Take: void
+ *  Returns: void
+ *  Reads the last completed adc conversion on the light channel.  Calculates the light value in mV and resistance
+ */
+void read_light(void){
+	// reset conversion done flag
 
+	// read values
+	(void)AD1_GetChanValue16(LIGHT, &Val[LIGHT]);
+	// assign values to local variables
+	light = Val[LIGHT] ;
+
+	// convert light reading from adc to mV
+	light_mV = light / ADC_DIVS;
+	light_ratio = light_mV / ADC_FULL_VOL;
+
+	// calculate the resistance value
+	resistance = (RES * light_ratio) / ( 1 - light_ratio);
+
+	// set lux_flag
+	lux_flag = 1;
+}
+
+/*
+ *  void read_temp(void)
+ *  Take: void
+ *  Returns: void
+ *  Reads the last completed adc conversion on the temperature channel.
+ *  Calculates the temperature value in mV, and converts this into C and F temps
+ */
+void read_temp(void){
+
+	// read values
+	(void)AD1_GetChanValue16(TEMP, &Val[TEMP]);
+	// assign values to local variables
+
+	temp_adc = Val[TEMP] ;
+
+	// convert temp_adc to mV by scaling 2^16 over 3.3 volts
+	temp_mV = temp_adc / ADC_DIVS;
+	temp_C = (temp_mV - 500) / 10;
+	temp_F = ((9 * temp_C) / 5) + 32;
+
+	// set the temp_flag to signal display to update
+	temp_flag = 1;
+
+}
+
+/*
+ *  void check_enc_pb(void)
+ *  Take: void
+ *  Returns: void
+ *  Reads in the current encoder push button state, and debounces.
+ */
+
+void check_enc_pb(void){
+	// ROTARY ENCODER PUSH BUTTON DEBOUNCE
+	// get the push button state
+	button_pressed = ENC_PB_GetVal();
+	// if button is pressed
+	if (button_pressed == 0){
+		// increment counter
+		count++;
+		// might need to guard against over-flow here, incase the button is held down and we keep incrementing
+	}
+	// wait for counter to count up to 30,000 (ie, debounced)
+	if (count >= 15000){
+		// update button status
+		button_pressed = ENC_PB_GetVal();
+		// if button is pressed, set the flag
+		if (button_pressed == 0)
+			enc_pb_flag = 1;
+		// if button is released
+		if (button_pressed == 1)
+		{
+			// clear the counter, the push button flag, and the hold flag
+			count = 0;
+			enc_pb_flag = 0;
+			hold = 0;
+		}
+	}
+	// if the push button flag is set and the hold flag is clear
+	if (enc_pb_flag == 1 && hold == 0){
+		// clear the pushbutton flag, toggle the mode, set the hold flag
+		enc_pb_flag = 0;
+		mode ^= 1;
+		hold = 1;
+		// flag the display to update temperature
+		temp_flag = 1;
+		lux_flag = 1;
+		// invert the display
+		//PDC1_SetMode(mode);
+	}
+	// END DEBOUNCE
+}
+
+/*
+ *  void update_temp_display(void)
+ *  Take: void
+ *  Returns: void
+ * 	Updates the LCD display with the latest temperature data.  Displays F or C, depending
+ * 	on latest PB toggle.  Displays temps with 2 decimal places
+ */
+void update_temp_display(void){
+	// Convert the int counter value to a string for writing to display
+	if (mode == FAREN){
+		// get the whole number portion of temperature float
+		temp_int = floorf(temp_F);
+		// get the first two decimal places
+		temp_dec = (temp_F - temp_int) * 100;
+		// write the temperature type to display
+		PDC1_SetPos(0,0);
+		PDC1_WriteString("Temp(F):");
+	}
+	if (mode == CELCIUS){
+		// get the whole number portion of temperature
+		temp_int = floorf(temp_C);
+		// get the first two decimal places of temperature
+		temp_dec = (temp_C - temp_int) * 100;
+		// write temperature type to display
+		PDC1_SetPos(0,0);
+		PDC1_WriteString("Temp(C):");
+	}
+	// convert the temperatures to strings
+	sprintf(int_str, "% d", (int)temp_int);
+	sprintf(dec_str, "%.2d", (int)temp_dec);
+	// try to lop off the leading zero
+	if (int_str[0] == "0"){
+		int_str[0] = " ";
+	}
+	// clear the old value
+	PDC1_ClearLine(1);
+	PDC1_ClearLine(2);
+	// set initial x position
+	x_pos = 0;
+	// write integer value
+	x_pos = PDC1_WriteStringBigger(int_str, x_pos, 1);
+	// write the decimal place
+	x_pos = PDC1_WriteStringBigger(".", x_pos, 1);
+	// write the decimals
+	x_pos = PDC1_WriteStringBigger(dec_str, x_pos, 1);
+}
+
+/*
+ *  void update_lux_display(void)
+ *  Take: void
+ *  Returns: void
+ *  Updates the LCD display with the latest luminance data.  Displays ADC reading, resistance,
+ *  and Lux values.
+ */
+void update_lux_display(void){
+	// WRITE LUMINANCE DATA
+
+	PDC1_ClearLine(3);
+	PDC1_ClearLine(4);
+	PDC1_ClearLine(5);
+
+	if (light > 40000){
+		PDC1_weather_image(NIGHT, 60, 3);
+	}
+	else if (light <= 40000 && light > 8000){
+		PDC1_weather_image(CLOUDY, 60, 3);
+	}
+	else if (light <= 8000){
+		PDC1_weather_image(SUNNY, 60, 3);
+	}
+	sprintf(lux_str, "%d", (int)light);
+	sprintf(res_str, "%d", (int)resistance);
+	PDC1_SetPos(0,3);
+	PDC1_WriteString("ADC:");
+	PDC1_WriteString(lux_str);
+	PDC1_SetPos(0,4);
+	PDC1_WriteString("Res:");
+	PDC1_WriteString(res_str);
+	PDC1_SetPos(0,5);
+	PDC1_WriteString("Lux:");
+	PDC1_WriteString("TBD");
+}
 /* END main */
 /*!
  ** @}
